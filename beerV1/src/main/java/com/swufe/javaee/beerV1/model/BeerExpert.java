41c9f72d76226595d62e5a3463dd14c87a5702fe
Li Chenjun <80444460+Jasonlee-19@users.noreply.github.com>
2021-06-27 10:05:30 +0800

    Add files via upload

---
+++ beerV1/src/main/java/com/swufe/javaee/beerV1/model/BeerExpert.java
@@ -0,0 +1,18 @@
+package com.swufe.javaee.beerV1.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BeerExpert {
+    public List<Beer> getBeers(String color) {
+        List<Beer> beers = new ArrayList<>();
+        if (color.equals("amber")) {
+            beers.add(new Beer("Jack Amber", 100.0));
+            beers.add(new Beer("Red Moose", 200.0));
+        } else {
+            beers.add(new Beer("Jali Pale Ale", 100.0));
+            beers.add(new Beer("Gout Stout", 200.0));
+        }
+        return beers;
+    }
+}
